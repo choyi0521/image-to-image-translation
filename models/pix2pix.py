b499cb859b6235056e26dc91483a41ab2a9a128b
youngin <choyi0521@gmail.com>
2020-07-18 03:58:09 +0900

    instance

--- models/pix2pix.py
+++ models/pix2pix.py
@@ -1,28 +1,36 @@
 import torch.nn as nn
 import torch
+import functools
 
 
 class Pix2Pix(nn.Module):
-    def __init__(self, inner_channels, dropout=0.5):
+    def __init__(self, inner_channels, dropout=0.5, norm='instance'):
         super().__init__()
-        self.generator = UNetGenerator(3, inner_channels, dropout=dropout)
-        self.discriminator = NLayerDiscriminator(6, inner_channels)
+
+        if norm == 'batch':
+            norm_layer = functools.partial(nn.BatchNorm2d, affine=True, track_running_stats=True)
+        elif norm == 'instance':
+            norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+        else:
+            raise Exception('No such norm')
+        self.generator = UNetGenerator(3, inner_channels, dropout=dropout, norm_layer=norm_layer)
+        self.discriminator = NLayerDiscriminator(6, inner_channels, norm_layer=norm_layer)
 
     def generate(self, x):
         return self.generator(x)
 
 
 class UNetGenerator(nn.Module):
-    def __init__(self, image_channels=3, inner_channels=64, n_layers=8, dropout=0.5):
+    def __init__(self, image_channels=3, inner_channels=64, n_layers=8, dropout=0.5, norm_layer=nn.InstanceNorm2d):
         super().__init__()
         assert n_layers >= 5
 
-        block = UNetSkipConnectionBlock(inner_channels*8, inner_channels*8, 'innermost')
+        block = UNetSkipConnectionBlock(inner_channels*8, inner_channels*8, 'innermost', norm_layer=norm_layer)
         for _ in range(n_layers-5):
-            block = UNetSkipConnectionBlock(inner_channels*8, inner_channels*8, 'middle', block, dropout)
-        block = UNetSkipConnectionBlock(inner_channels*4, inner_channels*8, 'middle', block, dropout)
-        block = UNetSkipConnectionBlock(inner_channels*2, inner_channels*4, 'middle', block, dropout)
-        block = UNetSkipConnectionBlock(inner_channels, inner_channels*2, 'middle', block, dropout)
+            block = UNetSkipConnectionBlock(inner_channels*8, inner_channels*8, 'middle', block, dropout, norm_layer=norm_layer)
+        block = UNetSkipConnectionBlock(inner_channels*4, inner_channels*8, 'middle', block, dropout, norm_layer=norm_layer)
+        block = UNetSkipConnectionBlock(inner_channels*2, inner_channels*4, 'middle', block, dropout, norm_layer=norm_layer)
+        block = UNetSkipConnectionBlock(inner_channels, inner_channels*2, 'middle', block, dropout, norm_layer=norm_layer)
         self.model = UNetSkipConnectionBlock(image_channels, inner_channels, 'outermost', block)
 
     def forward(self, x):
@@ -36,27 +44,34 @@ class UNetSkipConnectionBlock(nn.Module):
                  module_type,
                  submodule=None,
                  dropout=0.5,
+                 norm_layer=nn.InstanceNorm2d
                  ):
         super().__init__()
+        
         if module_type not in ['innermost', 'outermost', 'middle']:
             raise Exception('no such module type')
 
-        down_conv = nn.Conv2d(outer_channels, inner_channels, kernel_size=4, stride=2, padding=1, bias=False)
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+        
+        down_conv = nn.Conv2d(outer_channels, inner_channels, kernel_size=4, stride=2, padding=1, bias=use_bias)
         down_relu = nn.LeakyReLU(0.2, True)
-        down_norm = nn.BatchNorm2d(inner_channels)
+        down_norm = norm_layer(inner_channels)
 
         up_relu = nn.ReLU(True)
-        up_norm = nn.BatchNorm2d(outer_channels)
+        up_norm = norm_layer(outer_channels)
 
         self.outermost = module_type == 'outermost'
         if module_type == 'innermost':
-            up_conv = nn.ConvTranspose2d(inner_channels, outer_channels, kernel_size=4, stride=2, padding=1, bias=False)
+            up_conv = nn.ConvTranspose2d(inner_channels, outer_channels, kernel_size=4, stride=2, padding=1, bias=use_bias)
             modules = [down_relu, down_conv, up_relu, up_conv, up_norm]
         elif module_type == 'outermost':
             up_conv = nn.ConvTranspose2d(inner_channels*2, outer_channels, kernel_size=4, stride=2, padding=1)
             modules = [down_conv, submodule, up_relu, up_conv, nn.Tanh()]
         else:
-            up_conv = nn.ConvTranspose2d(inner_channels*2, outer_channels, kernel_size=4, stride=2, padding=1, bias=False)
+            up_conv = nn.ConvTranspose2d(inner_channels*2, outer_channels, kernel_size=4, stride=2, padding=1, bias=use_bias)
             modules = [down_relu, down_conv, down_norm, submodule, up_relu, up_conv, up_norm, nn.Dropout(dropout)]
 
         self. model = nn.Sequential(*modules)
@@ -68,20 +83,30 @@ class UNetSkipConnectionBlock(nn.Module):
 
 
 class NLayerDiscriminator(nn.Module):
-    def __init__(self, input_channels=6, inner_channels=64, n_layers=3):
+    def __init__(self,
+                 input_channels=6,
+                 inner_channels=64,
+                 n_layers=3,
+                 norm_layer=nn.InstanceNorm2d
+                 ):
         super().__init__()
+
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
 
         modules = [nn.Conv2d(input_channels, inner_channels, kernel_size=4, stride=2, padding=1), nn.LeakyReLU(0.2, True)]
         for i in range(n_layers-1):
             modules += [
-                nn.Conv2d(inner_channels*min(2**i, 8), inner_channels*min(2**(i+1), 8), kernel_size=4, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(inner_channels*min(2**(i+1), 8)),
+                nn.Conv2d(inner_channels*min(2**i, 8), inner_channels*min(2**(i+1), 8), kernel_size=4, stride=2, padding=1, bias=use_bias),
+                norm_layer(inner_channels*min(2**(i+1), 8)),
                 nn.LeakyReLU(0.2, True)
             ]
         modules += [
             nn.Conv2d(inner_channels * min(2 ** (n_layers-1), 8), inner_channels * min(2 ** n_layers, 8), kernel_size=4, stride=1,
-                      padding=1, bias=False),
-            nn.BatchNorm2d(inner_channels * min(2 ** n_layers, 8)),
+                      padding=1, bias=use_bias),
+            norm_layer(inner_channels * min(2 ** n_layers, 8)),
             nn.LeakyReLU(0.2, True),
             nn.Conv2d(inner_channels * min(2 ** n_layers, 8), 1, kernel_size=4, stride=1, padding=1)
         ]
